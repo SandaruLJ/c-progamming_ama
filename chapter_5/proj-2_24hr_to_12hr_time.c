cdd905bb69bde31d2fb729dc4a016cbf4e8d88a7
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2023-06-09 15:54:33 +0530

    Finish Programming Project 2 of Chapter 5

---
+++ chapter_5/proj-2_24hr_to_12hr_time.c
@@ -0,0 +1,23 @@
+/* Converts a 24-hour time to 12-hour form */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int hour, minute;
+    bool pm = false;
+
+    printf("Enter a 24-hour time: ");
+    scanf("%d :%d", &hour, &minute);
+
+    pm = (hour >= 12) ? true : false;
+
+    if (hour > 12)
+        hour -= 12;
+
+    printf("Equivalent 12-hour time: %.2d:%.2d ", hour, minute);
+    printf(pm ? "PM\n" : "AM\n");
+
+    return 0;
+}
