1f5bb097c0fc79d5617a5c394d3ec42dc250f179
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2023-06-11 17:35:39 +0530

    Finish Programming Project 6 of Chapter 6

---
+++ chapter_6/proj-6_even_squares.c
@@ -0,0 +1,16 @@
+/* Prints all even squares between 1 and n */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 2; i * i <= n; i += 2)
+        printf("%d\n", i * i);
+
+    return 0;
+}
