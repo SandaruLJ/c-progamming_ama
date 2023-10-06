01f28e02602462517829f348ffc5ead583297e9c
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2023-10-06 22:54:04 +0530

    Finish Programming Project 3 of Chapter 15

--- chapter_15/proj-3_qsort/qsort.c
+++ chapter_15/proj-3_qsort/qsort.c
@@ -1,11 +1,9 @@
 /* Sorts an array of integers using Quicksort algorithm */
 
 #include <stdio.h>
+#include "quicksort.h"
 
 #define N 10
-
-void quicksort(int a[], int low, int high);
-int split(int a[], int low, int high);
 
 int main(void)
 {
@@ -23,35 +21,4 @@ int main(void)
     printf("\n");
 
     return 0;
-}
-
-void quicksort(int a[], int low, int high)
-{
-    int middle;
-
-    if (low >= high) return;
-    middle = split(a, low, high);
-    quicksort(a, low, middle - 1);
-    quicksort(a, middle + 1, high);
-}
-
-int split(int a[], int low, int high)
-{
-    int part_element = a[low];
-
-    for (;;)
-    {
-        while (low < high && part_element <= a[high])
-            high--;
-        if (low >= high) break;
-        a[low++] = a[high];
-
-        while (low < high && a[low] <= part_element)
-            low++;
-        if (low >= high) break;
-        a[high--] = a[low];
-    }
-
-    a[high] = part_element;
-    return high;
 }
