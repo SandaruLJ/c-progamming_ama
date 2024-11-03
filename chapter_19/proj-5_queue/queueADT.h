31cc98770fe7bee71cfc4b04e008d22c2159d782
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2024-11-03 17:44:18 +0530

    Finish Programming Project 5 of Chapter 19

---
+++ chapter_19/proj-5_queue/queueADT.h
@@ -0,0 +1,19 @@
+#ifndef QUEUEADT_H
+#define QUEUEADT_H
+
+#include <stdbool.h>
+
+typedef struct queue_type *Queue;
+
+Queue q_create(void);
+void q_destroy(Queue);
+void q_make_empty(Queue);
+bool q_is_empty(Queue);
+bool q_is_full(Queue);
+int q_first(Queue);
+int q_last(Queue);
+void q_insert(Queue, int);
+int q_remove(Queue);
+
+#endif
+
